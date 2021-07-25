875c8f37e5253a9a9196cc92c6cc3049ef27d65c
Jeff Cela <cela.j@labrador-company.com>
2021-07-25 18:05:09 -0400

    completed functions for quiz in js file, this version is compliant with all acceptance criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,3 +62,128 @@ const quizQuestions = [{
     correctAnswer: "2"
 },
 ];
+
+var questionIndex = quizQuestions.length;
+var currentQuestionIndex = 0;
+var timeLeft = 70;
+var timerInterval;
+var score = 0;
+var correct;
+
+
+function startQuiz() {
+    gameoverDiv.style.display = "none";
+    startQuizDiv.style.display = "none";
+    generateQuizQuestion();
+    timerInterval = setInterval(function () {
+        timeLeft--;
+        quizClock.textContent = "Time left: " + timeLeft;
+        if (timeLeft === 0) {
+            clearInterval(timerInterval);
+            showScore();
+        }
+    }, 1000);
+    quizBody.style.display = "block";
+}
+
+function generateQuizQuestion() {
+    gameoverDiv.style.display = "none";
+    if (currentQuestionIndex === questionIndex) {
+        return showScore();
+    }
+    var currentQuestion = quizQuestions[currentQuestionIndex];
+    questionsEl.innerHTML = "<p>" + currentQuestion.question + "</p>";
+    button1.innerHTML = currentQuestion.choice1;
+    button2.innerHTML = currentQuestion.choice2;
+    button3.innerHTML = currentQuestion.choice3;
+    button4.innerHTML = currentQuestion.choice4;
+};
+
+function showScore() {
+    quizBody.style.display = "none"
+    gameoverDiv.style.display = "flex";
+    clearInterval(timerInterval);
+    inputName.value = "";
+    totalScoreEl.innerHTML = "You got " + score + " out of " + quizQuestions.length + " correct!";
+}
+
+submitScoreBtn.addEventListener("click", function highscore() {
+    if (inputName.value === "") {
+        alert("Input field cannot be blank");
+        return false;
+    } else {
+        var savedHighscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+        var currentUser = inputName.value.trim();
+        var currentHighscore = {
+            name: currentUser,
+            score: score
+        };
+        gameoverDiv.style.display = "none";
+        highscoreContainer.style.display = "flex";
+        scoreDiv.style.display = "block";
+        endGameBtns.style.display = "flex";
+
+        savedHighscores.push(currentHighscore);
+        localStorage.setItem("savedHighscores", JSON.stringify(savedHighscores));
+        generateHighscores();
+    }
+});
+
+function generateHighscores() {
+    displayName.innerHTML = "";
+    displayScore.innerHTML = "";
+    var highscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
+    for (i = 0; i < highscores.length; i++) {
+        var newNameSpan = document.createElement("li");
+        var newScoreSpan = document.createElement("li");
+        newNameSpan.textContent = highscores[i].name;
+        newScoreSpan.textContent = highscores[i].score;
+        displayName.appendChild(newNameSpan);
+        displayScore.appendChild(newScoreSpan);
+    }
+}
+
+function showHighscore() {
+    startQuizDiv.style.display = "none"
+    gameoverDiv.style.display = "none";
+    highscoreContainer.style.display = "flex";
+    scoreDiv.style.display = "block";
+    endGameBtns.style.display = "flex";
+    generateHighscores();
+}
+
+function clearScore() {
+    window.localStorage.clear();
+    displayName.textContent = "";
+    displayScore.textContent = "";
+}
+
+function replayQuiz() {
+    highscoreContainer.style.display = "none";
+    gameoverDiv.style.display = "none";
+    startQuizDiv.style.display = "flex";
+    timeLeft = 76;
+    score = 0;
+    currentQuestionIndex = 0;
+}
+
+function checkAnswer(answer) {
+    correct = quizQuestions[currentQuestionIndex].correctAnswer;
+
+    if (answer === correct && currentQuestionIndex !== questionIndex) {
+        score++;
+        currentQuestionIndex++;
+        generateQuizQuestion();
+    } 
+    else if (answer !== correct && currentQuestionIndex !== questionIndex) {
+        timeLeft = timeLeft - 10;
+        currentQuestionIndex++;
+        generateQuizQuestion();
+    } 
+    else {
+        showScore();
+    }
+}
+
+startQuizButton.addEventListener("click", startQuiz);
+highScoreButton.addEventListener("click", showHighscore);
